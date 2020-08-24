88c3c6ec8281c34e68624c928620f919e592db1f
Zavel Y Ogola <yveszavel@gmail.com>
2020-08-24 06:26:53 +0200

    Initial Setup + Updated the server.js file to include the folder path

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3033
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/dist/mtibu-frontend')));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/mtibu-frontend/' });
+    res.sendFile('index.html', { root: 'dist/mtibu-frontend/index.html' });
 });
 
 app.listen(port, () => {
