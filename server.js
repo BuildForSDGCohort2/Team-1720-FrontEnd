884ff777b203440f9d3acebc05b7eb7fe850deff
Zavel Y Ogola <yveszavel@gmail.com>
2020-08-24 05:45:17 +0200

    Initial Setup + Updated the server console.log error message

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.info(`Example app listening at http://localhost:${port}`);
 });
