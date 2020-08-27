3fd84a41209e98c75e5a7666698350b616699389
Zavel Y Ogola <yveszavel@gmail.com>
2020-08-27 08:10:37 +0200

    Deployment Management + Removed the app listening port and replaced with the express generator code

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/*', function(req, res) {
     res.sendFile('index.html', { root: 'dist/mtibu-frontend/index.html' });
 });
 
-app.listen(port, () => {
-    console.info(`Example app listening at http://localhost:${port}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
