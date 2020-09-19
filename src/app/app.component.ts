7e4dee590e7f3506ac491d3a9211a1721cce54ea
Zavel Y Ogola <yveszavel@gmail.com>
2020-09-19 13:28:23 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -10,12 +11,18 @@ export class AppComponent {
 
   cookieValue;
   userLoggedIn = false;
+  routerUrl;
 
-  constructor(private cookieService: CookieService) {
+  constructor(private cookieService: CookieService, private router: Router, public route: ActivatedRoute) {
     this.cookieService.set('mtibabu', JSON.stringify({user: ''}), 1, '/', 'localhost', true, 'Strict');
     this.cookieValue = JSON.parse(this.cookieService.get('mtibabu'));
     this.userLoggedIn = this.cookieValue.user !== undefined && this.cookieValue.user.length > 0 ? true : false;
     console.log(this.userLoggedIn);
     // console.log(this.cookieValue.user);
+    this.routerUrl = router.url;
+    router.events.subscribe((val) => {
+      // see also 
+      console.log(val instanceof NavigationEnd);
+    });
   }
 }
