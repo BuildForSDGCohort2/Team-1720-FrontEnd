29c805d57e68120696d6545988b1e0a835219ab3
Zavel Y Ogola <yveszavel@gmail.com>
2020-09-23 05:43:39 +0200

    + Added the drop down directive for the top navigation

---
+++ src/app/shared/directives/dropdown-expand.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DropdownExpandDirective } from './dropdown-expand.directive';
+
+describe('DropdownExpandDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DropdownExpandDirective();
+    expect(directive).toBeTruthy();
+  });
+});
