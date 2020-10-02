5fc48cbf8f1e1b96562407d59909d74ae7378a41
Zavel Y Ogola <yveszavel@gmail.com>
2020-10-02 07:59:05 +0200

    Payments + Added the pop up functions

--- src/app/shared/components/elements/page-footer-slide-out/page-footer-slide-out.component.ts
+++ src/app/shared/components/elements/page-footer-slide-out/page-footer-slide-out.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-page-footer-slide-out',
@@ -9,9 +12,25 @@ export class PageFooterSlideOutComponent implements OnInit {
 
   @Input() showElement: boolean;
   @Output() closeFooterSlideOut: EventEmitter<any> = new EventEmitter<any>();
+  closeResult = '';
+  medicalAidForm: FormGroup;
+  disputeForm: FormGroup;
 
-  constructor() {
-    console.log(this.showElement);
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder) {
+    this.disputeForm = this.formBuilder.group({
+      patient_full_name: ['', Validators.required],
+      patient_email: ['', Validators.required],
+      patient_mobile: ['', Validators.required],
+      patient_notes: [''],
+      patient_accepted_terms_and_conditions: [null, Validators.required]
+    });
+    this.medicalAidForm = this.formBuilder.group({
+      full_name: ['', Validators.required],
+      email: ['', Validators.required],
+      mobile: ['', Validators.required],
+      notes: [''],
+      accepted_terms_and_conditions: [null, Validators.required]
+    });
   }
 
   closeSlideOut(): any {
@@ -19,6 +38,26 @@ export class PageFooterSlideOutComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  open(content): any {
+    this.modalService.open(content,
+      { ariaLabelledBy: 'modal-basic-title', windowClass: 'modal-dialog-centered' }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult =
+          `Dismissed ${this.getDismissReason(reason)}`;
+      });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
