9989d8aa6539b036a614bbb23f9afdc6218061a2
Zavel Y Ogola <yveszavel@gmail.com>
2020-10-22 19:40:09 +0200

    Updating the profile contacts

--- src/app/shared/components/forms/financial-info/financial-info.component.ts
+++ src/app/shared/components/forms/financial-info/financial-info.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinancialInfoComponent implements OnInit {
 
+  public hasDoctorAdded = false;
+
   constructor() { }
 
   ngOnInit(): void {
